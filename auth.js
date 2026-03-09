e00ce8f86f3be126c76d625fc5fee4e37d6c6e35
SuchAndSuchOR <lerickpeluyera@gmail.com>
2026-03-09 12:43:34 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,45 +1,49 @@
 import { auth } from "./firebase.js";
 
 import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 
-export function setupAuth(onLogin,onLogout){
 
-  const emailInput = document.getElementById("email");
-  const passwordInput = document.getElementById("password");
+export function setupAuth(onLogin){
 
-  const registerBtn = document.getElementById("registerBtn");
-  const loginBtn = document.getElementById("loginBtn");
+const registerBtn = document.getElementById("registerBtn");
+const loginBtn = document.getElementById("loginBtn");
 
-  registerBtn.onclick = async ()=>{
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
 
-    const email = emailInput.value;
-    const password = passwordInput.value;
 
-    await createUserWithEmailAndPassword(auth,email,password);
+registerBtn.onclick = async ()=>{
 
-  };
+const email = emailInput.value;
+const pass = passwordInput.value;
 
-  loginBtn.onclick = async ()=>{
+await createUserWithEmailAndPassword(auth,email,pass);
 
-    const email = emailInput.value;
-    const password = passwordInput.value;
+};
 
-    await signInWithEmailAndPassword(auth,email,password);
 
-  };
+loginBtn.onclick = async ()=>{
 
-  onAuthStateChanged(auth,user=>{
+const email = emailInput.value;
+const pass = passwordInput.value;
 
-    if(user){
-      onLogin(user);
-    }else{
-      onLogout();
-    }
+await signInWithEmailAndPassword(auth,email,pass);
 
-  });
+};
+
+
+onAuthStateChanged(auth,(user)=>{
+
+if(user){
+
+onLogin(user);
+
+}
+
+});
 
 }
